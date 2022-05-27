536a92610ad44d977adcf6971fb8a0b631d13015
Anderson Pablo <anderson.pablo02@gmail.com>
2022-05-27 10:23:31 -0300

    create method post in API'

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
-  res.status(200).json({ message: 'this works!' });
+
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+
+    const newFeedBack = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText
+    }
+
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+
+    const data = JSON.parse(fileData);
+
+    data.push(newFeedBack);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+  } else {
+
+    res.status(200).json({ message: 'this works!' });
+  }
+
 }
 
 export default handler;
